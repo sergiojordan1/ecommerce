da39f20929e2054e18c91004961096e2c0a83276
Sérgio Jordan <sergiojordan@live.com>
2020-01-06 23:26:17 -0300

    Site Produtos

--- site.php
+++ site.php
@@ -3,11 +3,16 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\Model\Product;
 	
 $app->get('/', function() {
+
+	$products = Product::listAll();
     
 	$page = new Page();
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products" => Product::checkList($products)
+	]);
 
 });
 	
