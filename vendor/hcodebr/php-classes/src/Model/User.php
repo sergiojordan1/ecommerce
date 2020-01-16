33a7a89634951cc6dae20adea739279b039ab46f
Sérgio Jordan <sergiojordan@live.com>
2020-01-15 22:38:47 -0300

    Admin Categorias Busca e Paginaçao

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -145,7 +145,7 @@
 		 		":iduser"=>$this->getiduser(),
 		 		":desperson"=>utf8_decode($this->getdesperson()),
 		 		":deslogin"=>$this->getdeslogin(),
-		 		":despassword"=>User::getPasswordHash($this->getdespassword()),	
+		 		":despassword"=>$this->getdespassword(),	
 		 		":desemail"=>$this->getdesemail(),
 		 		":nrphone"=>$this->getnrphone(),
 		 		":inadmin"=>$this->getinadmin()
